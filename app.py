903f4fdb2089308c88664b358e273f1c44258e6d
rrc_w2024_jaskomal <jaskomalkaila2004@gmail.com>
2025-04-13 00:33:11 +0530

    Commit 6

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-from flask import Flask, render_template, request, redirect, session, url_for, flash
-from firebase_admin import credentials, firestore, initialize_app
-import firebase_admin
-from datetime import datetime
 
 # 🔐 Firebase setup
 cred = credentials.Certificate("firebase_config.json")
